f4ed05520984db90eafa76b02311807ae9e323f6
Tobias Weiss <tobias@tobias-weiss.org>
2022-06-19 15:58:00 +0200

    add sudoku

--- Assets/Whiteboard/Scripts/Whiteboard.cs
+++ Assets/Whiteboard/Scripts/Whiteboard.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class Whiteboard : MonoBehaviour
 {
-    public Texture2D texture;
-    public Vector2 textureSize = new Vector2(2048, 2048);
+    public Texture2D textureAsset;
+    [HideInInspector] public Vector2 textureSize;
+    [HideInInspector] public Texture2D texture;
 
     // Start is called before the first frame update
     void Start()
     {
         var r = GetComponent<Renderer>();
-        texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        if (texture == null)
+        {
+            textureSize = new Vector2(2048, 2048);
+            texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        }
+        else
+        {
+            // clone texture to not overwrite it
+            texture = Instantiate(textureAsset);
+            textureSize = new Vector2(texture.width, texture.height);
+        }
+
+        // apply texture to material
         r.material.mainTexture = texture;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
