5b7b947a657c713d6d827f151cd7d683c48ec88f
Tobias Weiss <tobias@tobias-weiss.org>
2022-06-19 15:04:17 +0200

    whiteboard works

--- Assets/Whiteboard/Scripts/WhiteboardMarker.cs
+++ Assets/Whiteboard/Scripts/WhiteboardMarker.cs
@@ -11,7 +11,7 @@ public class WhiteboardMarker : MonoBehaviour
     private Color[] _colors;
     private float _tipHeight;
     private RaycastHit _touch;
-    private Whiteboard _whiteboard; 
+    private Whiteboard _whiteboard;
     private Vector2 _touchPos, _lastTouchPos;
     private bool _touchLastFrame;
     private Quaternion _lastTouchRotation;
@@ -22,53 +22,53 @@ public class WhiteboardMarker : MonoBehaviour
         _renderer = _tip.GetComponent<Renderer>();
         _colors = Enumerable.Repeat(_renderer.material.color, _penSize * _penSize).ToArray();
         _tipHeight = _tip.localScale.y;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-       Draw(); 
+        Draw();
     }
 
     void Draw()
     {
-        if(Physics.Raycast(_tip.position, transform.up, out _touch, _tipHeight))
+        if (Physics.Raycast(_tip.position, _tip.transform.up, out _touch, _tipHeight))
         {
-                if(_touch.transform.CompareTag("Whiteboard"))
+            if (_touch.transform.CompareTag("Whiteboard"))
+            {
+                if (_whiteboard == null)
                 {
-                    if (_whiteboard == null)
+                    _whiteboard = _touch.transform.GetComponent<Whiteboard>();
+                }
+
+                _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
+
+                var x = (int)(_touchPos.x * _whiteboard.textureSize.x - (_penSize / 2));
+                var y = (int)(_touchPos.y * _whiteboard.textureSize.y - (_penSize / 2));
+
+                if (y < 0 || y > _whiteboard.textureSize.y || x < 0 || x > _whiteboard.textureSize.x) return;
+
+                if (_touchLastFrame)
+                {
+                    _whiteboard.texture.SetPixels(x, y, _penSize, _penSize, _colors);
+
+                    for (float f = 0.01f; f < 1.00f; f += 0.01f)
                     {
-                        _whiteboard = _touch.transform.GetComponent<Whiteboard>();
+                        var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
+                        var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
+                        _whiteboard.texture.SetPixels(lerpX, lerpY, _penSize, _penSize, _colors);
                     }
-
-                    _touchPos = new Vector2(_touch.textureCoord.x, _touch.textureCoord.y);
-
-                    var x = (int)(_touchPos.x * _whiteboard.textureSize.x - (_penSize/2));
-                    var y = (int)(_touchPos.y * _whiteboard.textureSize.y - (_penSize/2));
-
-                    if (y > 0 || y < _whiteboard.textureSize.y || x > 0 || x < _whiteboard.textureSize.x ) return;
-
-                    if(_touchLastFrame)
-                    {
-                        _whiteboard.texture.SetPixels(x, y, _penSize, _penSize, _colors);
-
-                        for (float f = 0.01f; f < 1.00f; f+=0.01f)
-                        {
-                            var lerpX = (int)Mathf.Lerp(_lastTouchPos.x, x, f);
-                            var lerpY = (int)Mathf.Lerp(_lastTouchPos.y, y, f);
-                            _whiteboard.texture.SetPixels(lerpX, lerpY, _penSize, _penSize, _colors);
-                        }
-                    }
-
                     transform.rotation = _lastTouchRotation;
                     _whiteboard.texture.Apply();
-
-                    _lastTouchPos = new Vector2(x, y);
-                    _lastTouchRotation = transform.rotation;
-                    _touchLastFrame = true;
-                    return;
                 }
+
+
+                _lastTouchPos = new Vector2(x, y);
+                _lastTouchRotation = transform.rotation;
+                _touchLastFrame = true;
+                return;
+            }
         }
 
         _whiteboard = null;
