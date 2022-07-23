d322057a550067e80e6833750b1f7f524c402d13
Tobias Weiss <tobias@tobias-weiss.org>
2022-07-23 20:45:50 +0200

    complete trace game

--- Assets/TraceGame/TraceGame.cs
+++ Assets/TraceGame/TraceGame.cs
@@ -7,18 +7,24 @@ using TMPro;
 public class TraceGame : MonoBehaviour
 {
     public TraceSphere TracePrefab;
+    private List<GameObject> sphereList = new();
     public GameObject AnswerPrefab;
+    private List<GameObject> answerList = new();
     public int InstanceCount;
     public float highlightTargetDuration;
-    public float duration;
+    public float playDuration = 10.0f;
+    public float showResultDuration = 3.0f;
     private Vector3 areaSize;
     private int targetId;
     private Button startButton;
+    private TextMeshProUGUI resultText;
 
     // Start is called before the first frame update
     void Start()
     {
         startButton = GameObject.Find("StartButton").GetComponent<Button>();
+        resultText = GameObject.Find("ResultText").GetComponent<TextMeshProUGUI>();
+        resultText.gameObject.SetActive(false);
     }
 
     public void StartTraceGame()
@@ -34,37 +40,38 @@ public class TraceGame : MonoBehaviour
         {
             InstantiateGameSpheres(i);
         }
-        StartCoroutine(StopGame(duration));
+        StartCoroutine(EvaluateGame(playDuration));
     }
 
     void InstantiateGameSpheres(int id)
     {
-        TraceSphere newUnit = Instantiate(TracePrefab, transform);
-        newUnit.SetAreaSize(areaSize);
-        newUnit.Id = id;
-        newUnit.name = "Sphere " + id.ToString();
+        TraceSphere sphere = Instantiate(TracePrefab, transform);
+        sphere.SetAreaSize(areaSize);
+        sphere.Id = id;
+        sphere.name = "Sphere " + id.ToString();
+        sphereList.Add(sphere.gameObject);
 
         Vector3 position = new Vector3(
         Random.Range(-areaSize.x, areaSize.x),
         Random.Range(-areaSize.y, areaSize.y),
         Random.Range(-areaSize.z, areaSize.z));
-        newUnit.transform.localPosition = position;
-        if (newUnit.Id == targetId)
+        sphere.transform.localPosition = position;
+        if (sphere.Id == targetId)
         {
-            StartCoroutine(HighlightTarget(newUnit, highlightTargetDuration));
+            StartCoroutine(HighlightTarget(sphere, highlightTargetDuration));
         }
     }
 
-    IEnumerator HighlightTarget(TraceSphere newUnit, float duration)
+    IEnumerator HighlightTarget(TraceSphere newUnit, float playDuration)
     {
         Color origColor = newUnit.GetComponent<MeshRenderer>().material.color;
         newUnit.GetComponent<MeshRenderer>().material.color = Color.red;
-        yield return new WaitForSeconds(duration);
+        yield return new WaitForSeconds(playDuration);
         newUnit.GetComponent<MeshRenderer>().material.color = origColor;
     }
-    IEnumerator StopGame(float duration)
+    IEnumerator EvaluateGame(float playDuration)
     {
-        yield return new WaitForSeconds(duration);
+        yield return new WaitForSeconds(playDuration);
         foreach (TraceSphere unit in GetComponentsInChildren<TraceSphere>())
         {
             unit.ToggleMove = false;
@@ -73,7 +80,6 @@ public class TraceGame : MonoBehaviour
             idText.enabled = true;
         }
         CreateAnswerButtons();
-        startButton.interactable = true;
     }
 
     void CreateAnswerButtons()
@@ -81,17 +87,41 @@ public class TraceGame : MonoBehaviour
         var canvas = GameObject.Find("Canvas");
         for (int i = 0; i < InstanceCount; ++i)
         {
-            GameObject newAnswer = Instantiate(AnswerPrefab, canvas.transform);
-            newAnswer.transform.localPosition = new Vector3(-100 + i * 50f, -50f, 0);
-            newAnswer.name = $"Answer {i}";
-            var answerText = newAnswer.GetComponentInChildren<TextMeshProUGUI>();
+            GameObject answerButton = Instantiate(AnswerPrefab, canvas.transform);
+            answerButton.transform.localPosition = new Vector3(-100 + i * 50f, -50f, 0);
+            answerButton.name = $"Answer {i}";
+            var answerText = answerButton.GetComponentInChildren<TextMeshProUGUI>();
             answerText.text = i.ToString();
+            var button = answerButton.GetComponent<Button>();
+            int index = i; // cannot use i here: https://answers.unity.com/questions/1288510/buttononclickaddlistener-how-to-pass-parameter-or.html
+            button.onClick.AddListener(delegate { onAnswerClick(index); });
+            answerList.Add(answerButton);
         }
-
     }
 
-    void Update()
+    void onAnswerClick(int index)
     {
+        foreach (GameObject answer in answerList)
+            Destroy(answer);
+        answerList = new();
+        foreach (GameObject sphere in sphereList)
+            Destroy(sphere);
+        sphereList = new();
+
+        if (index == targetId)
+            resultText.text = "Correct answer!";
+        else
+            resultText.text = $"Wrong answer! Sphere {targetId} was correct!";
+
+        resultText.gameObject.SetActive(true);
+        StartCoroutine(AllowRestart(showResultDuration));
+    }
+
+    IEnumerator AllowRestart(float showResultsDuration)
+    {
+        yield return new WaitForSeconds(showResultsDuration);
+        resultText.gameObject.SetActive(false);
+        startButton.interactable = true;
 
     }
 }
